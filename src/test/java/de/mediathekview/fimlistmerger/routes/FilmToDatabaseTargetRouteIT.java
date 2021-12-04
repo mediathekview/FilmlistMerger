44339e593114bbd1ec3710e1865d9648306298f6
Nicklas Wiegandt <nicklas@wiegandt.eu>
2021-12-04 12:25:00 +0100

    Fix test film count

--- src/test/java/de/mediathekview/fimlistmerger/routes/FilmToDatabaseTargetRouteIT.java
+++ src/test/java/de/mediathekview/fimlistmerger/routes/FilmToDatabaseTargetRouteIT.java
@@ -66,6 +66,8 @@ class FilmToDatabaseTargetRouteIT {
             "FilmToDatabaseTargetRouteIT",
             LocalDateTime.now(),
             Duration.ofMinutes(18));
+    //500 signs lorem ipsum
+    filmToSave.setBeschreibung("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et e");
 
     // when
     template.sendBody(FilmToDatabaseTargetRoute.ROUTE_FROM, filmToSave);
