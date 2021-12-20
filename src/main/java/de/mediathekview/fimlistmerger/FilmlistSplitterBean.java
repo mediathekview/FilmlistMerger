6c1a4ff8120c879869aaaa1166c471d100480b84
Nicklas Wiegandt <nicklas@wiegandt.eu>
2021-12-20 19:15:21 +0100

    Change split result from list to set to remove duplicates

--- src/main/java/de/mediathekview/fimlistmerger/FilmlistSplitterBean.java
+++ src/main/java/de/mediathekview/fimlistmerger/FilmlistSplitterBean.java
@@ -3,12 +3,12 @@ package de.mediathekview.fimlistmerger;
 import de.mediathekview.mlib.daten.Film;
 import de.mediathekview.mlib.daten.Filmlist;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FilmlistSplitterBean {
   @SuppressWarnings("unused")
-  public List<Film> splitBody(Filmlist filmlist) {
-    return new ArrayList<>(filmlist.getFilms().values());
+  public Set<Film> splitBody(Filmlist filmlist) {
+    return new HashSet<>(filmlist.getFilms().values());
   }
 }
