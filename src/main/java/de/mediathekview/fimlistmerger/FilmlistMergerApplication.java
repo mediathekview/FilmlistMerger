54430cca68b821a315e1e69149c28ddb9432e198
Nicklas Wiegandt <nicklas@wiegandt.eu>
2022-02-25 00:56:14 +0100

    Add config value for db startup timeout

--- src/main/java/de/mediathekview/fimlistmerger/FilmlistMergerApplication.java
+++ src/main/java/de/mediathekview/fimlistmerger/FilmlistMergerApplication.java
@@ -1,6 +1,7 @@
 package de.mediathekview.fimlistmerger;
 
 import liquibase.Liquibase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,31 +14,33 @@ import java.util.stream.Stream;
 
 @SpringBootApplication
 public class FilmlistMergerApplication {
-    public static void main(String[] args) {
-		SpringApplication.run(FilmlistMergerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FilmlistMergerApplication.class, args);
+  }
 
-	@Bean
-	public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
-		var dsv = new DatabaseStartupValidator();
-		dsv.setDataSource(dataSource);
-		return dsv;
-	}
+  @Bean
+  public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+    return bf -> {
+      // Let beans that need the database depend on the DatabaseStartupValidator
+      // like the JPA EntityManagerFactory or Flyway
+      String[] liquibase = bf.getBeanNamesForType(Liquibase.class);
+      Stream.of(liquibase)
+          .map(bf::getBeanDefinition)
+          .forEach(it -> it.setDependsOn("databaseStartupValidator"));
 
-	@Bean
-	public static BeanFactoryPostProcessor dependsOnPostProcessor() {
-		return bf -> {
-			// Let beans that need the database depend on the DatabaseStartupValidator
-			// like the JPA EntityManagerFactory or Flyway
-			String[] liquibase = bf.getBeanNamesForType(Liquibase.class);
-			Stream.of(liquibase)
-					.map(bf::getBeanDefinition)
-					.forEach(it -> it.setDependsOn("databaseStartupValidator"));
+      String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
+      Stream.of(jpa)
+          .map(bf::getBeanDefinition)
+          .forEach(it -> it.setDependsOn("databaseStartupValidator"));
+    };
+  }
 
-			String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
-			Stream.of(jpa)
-					.map(bf::getBeanDefinition)
-					.forEach(it -> it.setDependsOn("databaseStartupValidator"));
-		};
-	}
+  @Bean
+  public DatabaseStartupValidator databaseStartupValidator(
+      DataSource dataSource, @Value("${filmlistmerger.database.startup.timeout}") String timeout) {
+    var dsv = new DatabaseStartupValidator();
+    dsv.setDataSource(dataSource);
+    dsv.setTimeout(Integer.parseInt(timeout));
+    return dsv;
+  }
 }
