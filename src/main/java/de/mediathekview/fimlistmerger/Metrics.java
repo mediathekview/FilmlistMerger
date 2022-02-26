57dbe12d0ef198a7aa6ce813ce45b1bb10c73aad
Nicklas Wiegandt <nicklas@wiegandt.eu>
2022-02-26 15:54:19 +0100

    Add micrometer influxdb metrics

--- src/main/java/de/mediathekview/fimlistmerger/Metrics.java
+++ src/main/java/de/mediathekview/fimlistmerger/Metrics.java
@@ -1,9 +1,18 @@
 package de.mediathekview.fimlistmerger;
 
 public enum Metrics {
-    COUNTER_READ_FILMS_OLD_FORMAT(Types.METRIC_TYPE_COUNTER,"counter.films.oldformat"),
-    COUNTER_READ_FILMS_NEW_FORMAT(Types.METRIC_TYPE_COUNTER,"counter.films.newformat"),
-    COUNTER_FILMS_SAVED(Types.METRIC_TYPE_COUNTER,"counter.films.saved");
+    COUNTER_READ_FILMS_OLD_FORMAT_MAX(Types.METRIC_TYPE_COUNTER,"filmlistmerger.films.read.format.old.max?increment=1&tags=app=filmlistmerger"),
+    COUNTER_READ_FILMS_NEW_FORMAT_MAX(Types.METRIC_TYPE_COUNTER,"filmlistmerger.films.read.format.new.max?increment=1&tags=app=filmlistmerger"),
+    COUNTER_FILMS_SAVED_CURRENT(Types.METRIC_TYPE_COUNTER,"filmlistmerger.films.saved.current?increment=1&tags=app=filmlistmerger"),
+    SUMMARY_MERGED_FILMS_WRITE(Types.METRIC_TYPE_SUMMARY,"filmlistmerger.films.write.merged?tags=app=filmlistmerger"),
+    TIMER_READ_FILMS_OLD_FORMAT_START(Types.METRIC_TYPE_TIMER,"filmlistmerger.films.read.format.old?action=start&tags=app=filmlistmerger"),
+    TIMER_READ_FILMS_OLD_FORMAT_STOP(Types.METRIC_TYPE_TIMER,"filmlistmerger.films.read.format.old?action=stop&tags=app=filmlistmerger"),
+    TIMER_READ_FILMS_NEW_FORMAT_START(Types.METRIC_TYPE_TIMER,"filmlistmerger.films.read.format.new?action=start&tags=app=filmlistmerger"),
+    TIMER_READ_FILMS_NEW_FORMAT_STOP(Types.METRIC_TYPE_TIMER,"filmlistmerger.films.read.format.new?action=stop&tags=app=filmlistmerger"),
+    TIMER_WRITE_MERGED_FILMS_START(Types.METRIC_TYPE_TIMER,"filmlistmerger.films.write.merged?action=start&tags=app=filmlistmerger"),
+    TIMER_WRITE_MERGED_FILMS_STOP(Types.METRIC_TYPE_TIMER,"filmlistmerger.films.write.merged?action=stop&tags=app=filmlistmerger"),
+    TIMER_WRITE_FILM_START(Types.METRIC_TYPE_TIMER,"filmlistmerger.film.write?action=start&tags=app=filmlistmerger"),
+    TIMER_WRITE_FILM_STOP(Types.METRIC_TYPE_TIMER,"filmlistmerger.film.write?action=stop&tags=app=filmlistmerger");
 
     private final String metricType;
     private final String metricName;
@@ -27,6 +36,8 @@ public enum Metrics {
     }
 
     private static class Types {
-        private static final String METRIC_TYPE_COUNTER = "metrics:counter";
+        private static final String METRIC_TYPE_COUNTER = "micrometer:counter";
+        private static final String METRIC_TYPE_SUMMARY = "micrometer:summary";
+        private static final String METRIC_TYPE_TIMER = "micrometer:timer";
     }
 }
--- src/main/java/de/mediathekview/fimlistmerger/routes/ReadOldFilmlistFormatRoute.java
+++ src/main/java/de/mediathekview/fimlistmerger/routes/ReadOldFilmlistFormatRoute.java
@@ -5,6 +5,7 @@ import de.mediathekview.fimlistmerger.Metrics;
 import de.mediathekview.fimlistmerger.dataformat.oldfilmlist.OldFilmlistDataFormat;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.micrometer.MicrometerConstants;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,16 +19,24 @@ public class ReadOldFilmlistFormatRoute extends RouteBuilder {
   public void configure() {
     from(DIRECT_READ_OLD_FILMLIST)
         .routeId(ROUTE_ID)
+        .to(Metrics.TIMER_READ_FILMS_OLD_FORMAT_START.toString())
+        .onCompletion()
+        .to(Metrics.TIMER_READ_FILMS_OLD_FORMAT_STOP.toString())
+        .end()
         .unmarshal(new OldFilmlistDataFormat())
         .log(
             LoggingLevel.INFO,
             "Finished reading old filmlist with ${body.getFilms().keySet().size()} entries, now splitting & importing.")
+        .setHeader(
+            MicrometerConstants.HEADER_COUNTER_INCREMENT,
+            simple("${body.getFilms().keySet().size()}"))
+        .choice()
+        .to(Metrics.COUNTER_READ_FILMS_OLD_FORMAT_MAX.toString())
         .split()
         .method(FilmlistSplitterBean.class)
         .streaming()
         .parallelProcessing()
         .log(LoggingLevel.DEBUG, "Old filmlist film body: ${body}")
-        .to(Metrics.COUNTER_READ_FILMS_OLD_FORMAT.toString())
         .to(FilmToDatabaseTargetRoute.ROUTE_FROM)
         .id(SINGLE_OLD_FORMAT_FILM_ROUTING_TARGET);
   }
